a5e125b61b97df8d8fa4c7c32deb9ab19d76c440
gioaprilino <86983840+gioaprilino@users.noreply.github.com>
2025-06-27 15:23:39 +0000

    Apply automatic changes

--- app/Filament/Hrd/Resources/JobVacancyResource/Pages/CreateJobVacancy.php
+++ app/Filament/Hrd/Resources/JobVacancyResource/Pages/CreateJobVacancy.php
@@ -3,7 +3,6 @@
 namespace App\Filament\Hrd\Resources\JobVacancyResource\Pages;
 
 use App\Filament\Hrd\Resources\JobVacancyResource;
-use Filament\Actions;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreateJobVacancy extends CreateRecord
